54fe57a7f096c32a5b1e3e83248e132fe946151b
SastreMax <sastremaximiliano@gmail.com>
2024-09-08 18:58:24 -0300

    Se crea el componente NavBar
    . Dentro de src, se crea components, y dentro el archivo NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+function NavBar() {
+    return (
+        <nav>
+            <h1></h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    )
+}
