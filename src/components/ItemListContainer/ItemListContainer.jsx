c60af68a607639dd0c29c8082a1d399bd4a210a6
SastreMax <sastremaximiliano@gmail.com>
2024-09-18 16:30:39 -0300

    ItemListContainer
    - Se hacen retoques visuales en white y black version

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -13,69 +13,77 @@ import { BsArrowUpRight } from "react-icons/bs";
 
 const ItemListContainer = ({ products }) => {
 
-  const boxShadow = useColorModeValue("6px 6px 0 black");
+  const borderColor = useColorModeValue("black", "white");
+  const boxShadow = useColorModeValue("6px 6px 0 black", "6px 6px 0 white");
+  const bgColor = useColorModeValue("white", "gray.200");  
 
-  return products.map((item) => {
-    return (
-      <Center py={6} key={item.id}>
-        <Box
-          w="xs"
-          rounded={"lg"}
-          my={5}
-          mx={[0, 5]}
-          overflow={"hidden"}
-          bg="white"
-          borderWidth={"2px"}
-          borderColor="black"          
-          boxShadow={boxShadow}
-        >
-          <Box h={"200px"} borderBottom={"1px"} borderColor="black">
-            <Img
-              src={
-                "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-              }
-              roundedTop={"sm"}
-              objectFit="cover"
-              h="full"
-              w="full"
-              alt={"Blog Image"}
-            />
-          </Box>
-          <Box p={4}>            
-            <Heading color={"black"} fontSize={"2xl"} noOfLines={1}>
-              { item.name }
-            </Heading>
-            <Text color={"gray.500"} noOfLines={2}>
-              { item.description }
-            </Text>            
-            <Box bg="black" display={'inline-block'} px={2} py={1} color="white" mb={2}>
-              <Text fontSize={"xs"} fontWeight={"medium"} color={"white"} noOfLines={2}>
-              ${ item.price }
-              </Text>              
+  return (
+    <Flex
+      flexWrap="wrap" 
+      justifyContent="center"
+      gap={6}
+    >
+      {products.map((item) => (
+        <Center py={6} key={item.id}>
+          <Box
+            w="xs"
+            rounded={"lg"}
+            my={5}
+            mx={[0, 5]}
+            overflow={"hidden"}
+            bg={bgColor}
+            borderWidth={"2px"}
+            borderColor={borderColor}        
+            boxShadow={boxShadow}
+          >
+            <Box h={"200px"} borderBottom={"1px"} borderColor={borderColor}>
+              <Img
+                src={
+                  "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                }
+                roundedTop={"sm"}
+                objectFit="cover"
+                h="full"
+                w="full"
+                alt={"Blog Image"}
+              />
             </Box>
-            <Text color={"gray.500"} noOfLines={2}>
-                Stock disponible: { item.stock > 0 ? item.stock : "Agotado"}
-              </Text>
+            <Box p={4}>            
+              <Heading color={"black"} fontSize={"2xl"} noOfLines={1}>
+                { item.title }
+              </Heading>
+              <Text color={"gray.500"} noOfLines={2}>
+                { item.description }
+              </Text>            
+              <Box bg={bgColor} display={'inline-block'} px={2} py={1} color="white" mb={2}>
+                <Text fontSize={"xs"} fontWeight={"medium"} color={"black"} border="black" noOfLines={2}>
+                ${ item.price }
+                </Text>              
+              </Box>
+                <Text color={"gray.500"} noOfLines={2}>
+                  Stock disponible: { item.stock > 0 ? item.stock : "Agotado"}
+                </Text>
+            </Box>
+            <HStack borderTop={"1px"} color="black">
+              <Flex
+                p={4}
+                alignItems="center"
+                justifyContent={"space-between"}
+                roundedBottom={"sm"}
+                cursor={"pointer"}
+                w="full"
+              >
+                <Text fontSize={"md"} fontWeight={"semibold"}>
+                  Ver más
+                </Text>
+                <BsArrowUpRight />
+              </Flex>
+            </HStack>
           </Box>
-          <HStack borderTop={"1px"} color="black">
-            <Flex
-              p={4}
-              alignItems="center"
-              justifyContent={"space-between"}
-              roundedBottom={"sm"}
-              cursor={"pointer"}
-              w="full"
-            >
-              <Text fontSize={"md"} fontWeight={"semibold"}>
-                Ver más
-              </Text>
-              <BsArrowUpRight />
-            </Flex>
-          </HStack>
-        </Box>
-      </Center>
-    );
-  });
+        </Center>
+      ))};
+    </Flex>
+  );
 };
 
 ItemListContainer.propTypes = {
