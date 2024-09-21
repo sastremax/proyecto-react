34bdc161cf9625ae9e7c63c0309e8ec3b288b366
SastreMax <sastremaximiliano@gmail.com>
2024-09-21 17:11:18 -0300

    ItemListContainer - home
    - Se da un wrap a los box para que queden mejor presentados. Se da un color en black view y otro color en vista normal.-
    - Se agrega un card mas. Se corrigen los tittle.-

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -12,17 +12,12 @@ import {
 import { BsArrowUpRight } from "react-icons/bs";
 
 const ItemListContainer = ({ products }) => {
-
   const borderColor = useColorModeValue("black", "white");
   const boxShadow = useColorModeValue("6px 6px 0 black", "6px 6px 0 white");
-  const bgColor = useColorModeValue("white", "gray.200");  
+  const bgColor = useColorModeValue("white", "gray.200");
 
   return (
-    <Flex
-      flexWrap="wrap" 
-      justifyContent="center"
-      gap={6}
-    >
+    <Flex flexWrap="wrap" justifyContent="center" gap={6}>
       {products.map((item) => (
         <Center py={6} key={item.id}>
           <Box
@@ -33,7 +28,7 @@ const ItemListContainer = ({ products }) => {
             overflow={"hidden"}
             bg={bgColor}
             borderWidth={"2px"}
-            borderColor={borderColor}        
+            borderColor={borderColor}
             boxShadow={boxShadow}
           >
             <Box h={"200px"} borderBottom={"1px"} borderColor={borderColor}>
@@ -48,21 +43,34 @@ const ItemListContainer = ({ products }) => {
                 alt={"Blog Image"}
               />
             </Box>
-            <Box p={4}>            
+            <Box p={4}>
               <Heading color={"black"} fontSize={"2xl"} noOfLines={1}>
-                { item.title }
+                {item.title}
               </Heading>
               <Text color={"gray.500"} noOfLines={2}>
-                { item.description }
-              </Text>            
-              <Box bg={bgColor} display={'inline-block'} px={2} py={1} color="white" mb={2}>
-                <Text fontSize={"xs"} fontWeight={"medium"} color={"black"} border="black" noOfLines={2}>
-                ${ item.price }
-                </Text>              
-              </Box>
-                <Text color={"gray.500"} noOfLines={2}>
-                  Stock disponible: { item.stock > 0 ? item.stock : "Agotado"}
+                {item.description}
+              </Text>
+              <Box
+                bg={bgColor}
+                display={"inline-block"}
+                px={2}
+                py={1}
+                color="white"
+                mb={2}
+              >
+                <Text
+                  fontSize={"xs"}
+                  fontWeight={"medium"}
+                  color={"black"}
+                  border="black"
+                  noOfLines={2}
+                >
+                  ${item.price}
                 </Text>
+              </Box>
+              <Text color={"gray.500"} noOfLines={2}>
+                Stock disponible: {item.stock > 0 ? item.stock : "Agotado"}
+              </Text>
             </Box>
             <HStack borderTop={"1px"} color="black">
               <Flex
@@ -81,7 +89,8 @@ const ItemListContainer = ({ products }) => {
             </HStack>
           </Box>
         </Center>
-      ))};
+      ))}
+      ;
     </Flex>
   );
 };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,31 +5,38 @@ const Home = () => {
     const productsData = [
         {
         id: 1,
-        title: "zapatilla adidas",
-        description: "zapatilla marca adidas talle 42",
+        title: "Zapatilla Adidas",
+        description: "zapatilla marca Adidas talle 42",
         price: 120,
         stock: 10,
         },
         {
         id: 2,
-        title: "zapatilla topper",
+        title: "Zapatilla Topper",
         description: "zapatilla marca topper talle 42",
         price: 100,
         stock: 10,
         },
         {
         id: 3,
-        title: "zapatilla kiker",
+        title: "Zapatilla Kiker",
         description: "zapatilla marca kiker talle 42",
         price: 110,
         stock: 10,
         },
         {
         id: 4,
-        title: "zapatilla nike",
+        title: "Zapatilla Nike",
         description: "zapatilla marca nike talle 42",
         price: 140,
         stock: 10,
+        },
+        {
+        id: 5,
+        title: "Zapatilla Trucha",
+        description: "zapatilla marca trucha talle 42",
+        price: 70,
+        stock: 8,
         }
     ];
 
