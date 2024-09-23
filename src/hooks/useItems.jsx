72064309b67610c24af93399f35879db1ed5b14e
SastreMax <sastremaximiliano@gmail.com>
2024-09-22 23:54:43 -0300

    useItems - home - ProptypesClass
    - En ProptypesClass se crean los archivos index.js y ProptypesClass.jsx.-
    - En home se importa hooks.-
    - Se crea useItems.-

---
+++ src/hooks/useItems.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { getAllProducts } from "../services/products.service";
+
+export const useItems = () => {
+    const [productsData, setProductsData] = useState([]);
+    const [loading, setLoading] = useState(true);   
+
+    useEffect(() => {
+        getAllProducts()
+        .then((res) => {           
+            setProductsData(res.data.products);
+        })
+        .catch((error) => {
+            console.log("Error al obtener los productos", error);       
+        })
+        .finally(() => setLoading(false));
+    }, []);
+
+    return { productsData, loading };
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,26 +1,7 @@
 import { ItemListContainer, Loader } from "../components";
-import { useEffect, useState } from "react";
-import { getAllProducts } from "../services/products.service";
+import { useItems } from "../hooks";
 
 export const Home = () => {
-
-    const [productsData, setProductsData] = useState([]);
-    const [loading, setLoading] = useState(true);   
-
-    useEffect(() => {
-        getAllProducts()
-        .then((res) => {           
-            setProductsData(res.data.products);
-        })
-        .catch((error) => {
-            console.log("Error al obtener los productos", error);       
-        })
-        .finally(() => setLoading(false));
-    }, []);
-
-    return loading ? (
-        <Loader />
-    ) : (
-        <ItemListContainer products={productsData} />
-    );
+    const { productsData, loading } = useItems(); 
+    return loading ? <Loader /> : <ItemListContainer products={productsData} />;
 };
