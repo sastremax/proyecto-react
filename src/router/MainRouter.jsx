56bbadddd5d1285993b70bfde9679072e29859c1
SastreMax <sastremaximiliano@gmail.com>
2024-09-30 13:10:18 -0300

    temp-pages
    se renombra de forma temporal pages

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Category, Home, ItemDetail, NotFound } from "../pages";
+import { Category, Home, ItemDetail, NotFound } from "../temp-pages";
 import { NavBar } from "../components";
 import { AllProducts } from "../components/AllProducts";
 
