2363619823fd271910058c51af58180d1c225bea
SastreMax <sastremaximiliano@gmail.com>
2024-09-30 12:20:44 -0300

    cartPage
    Se quita cartPage

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,20 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import { Category, Home, ItemDetail, NotFound, CartPage } from "../Pages";
+import { Category, Home, ItemDetail, NotFound } from "../Pages";
 import { NavBar } from "../components";
 import { AllProducts } from "../components/AllProducts";
 
 export const MainRouter = () => {
-  return (
-      <>
-          <NavBar />
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/item/:id" element={<ItemDetail />} />
-              <Route path="/category/:categoryId" element={<Category />} />
-              <Route path="/productos" element={<AllProducts />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="*" element={<NotFound />} />
-          </Routes>
-      </>
-  );
+    return (
+        <>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/item/:id" element={<ItemDetail />} />
+                <Route path="/category/:categoryId" element={<Category />} />
+                <Route path="/productos" element={<AllProducts />} />                
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
+    );
 };
