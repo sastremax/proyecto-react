2e3edfe06162878a17300b2203157b6f8ea377ad
SastreMax <sastremaximiliano@gmail.com>
2024-11-04 23:44:48 -0300

    se quitan comentarios

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,7 @@
 import { Box } from "@chakra-ui/react";
 
-// Componente contenedor
-// Tenemos el navbar porque es un componente estatico
-// children recibe los componentes como si fuese un array [componente1, 2, 3, 4]
-// HOC ---> Higher Order Component (similar a HOF Higher Order Function)
 const MainLayout = ({ children }) => {
-  return (
-    <Box>
-      {children}
-    </Box>
-  );
+    return <Box>{children}</Box>;
 };
 
 export default MainLayout;
